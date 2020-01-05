a7b3e971e3081a07fed6a7f441724d503f66fb6d
Kieran Davies <32366557+A-Stranger1k@users.noreply.github.com>
2020-01-05 17:59:27 +0000

    remove sensitive data

--- ihsa_base/global/connect.php
+++ ihsa_base/global/connect.php
@@ -1,9 +1,9 @@
 <?php
 // MySQL Settings
 define("DB_HOST", "localhost");
-define("DB_USER", "kmyywbsn_appsadmin");
-define("DB_PASS", "lD(2Vv&cYYzz");
-define("DB_NAME", "kmyywbsn_staffapp");
+define("DB_USER", "Your DB User");
+define("DB_PASS", "Your DB User's Password");
+define("DB_NAME", "Your DB Name");
 require_once 'functions.php';
 
 // Do Not Edit Below --- SERIOUSLY DON'T TOUCH THIS STUFF.
@@ -15,10 +15,12 @@ try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, $pdoOptions);
 }
 
+// Catches errors connecting to the database
 catch(Exception $e) {
     die('<strong>Fatal Error while connecting to the database. Please refresh and try again, or check back later!</strong>');
 }
 
+// Checks if the server has allow_url_fopen enabled
 if(!ini_get('allow_url_fopen')) {
     die('allow_url_fopen is disabled. Please enable it in your host\'s php settings.');
 }
