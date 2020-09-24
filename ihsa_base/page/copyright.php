073ed1348bed38958ce148220d0b4043d0af6965
Kieran Davies <kd0104@outlook.com>
2020-09-24 18:46:15 +0100

    bye bye

--- ihsa_base/page/copyright.php
+++
@@ -1,9 +0,0 @@
-<div class="lime-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <span class="footer-text float-left" id="kep">©<?php echo $year; ?> <a href="https://intelligencehosting.com">Intelligence Hosting</a></span>
-            </div>
-        </div>
-    </div>
-</div>
